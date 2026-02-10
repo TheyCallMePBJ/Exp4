7019bbe02284ebbac6aabf72db8144f0008c5bae
Manomay Gadiyar <manomay.gadiyar@gmail.com>
2026-02-10 11:54:54 +0530

    Added division

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from add import add
 from sub import sub
 from mul import mul
+from div import div
 
 def main():
     a = 10
@@ -10,6 +11,7 @@ def main():
     print("Addition: (a+b)", add(a,b))
     print("Subtraction (a-b):", sub(a,b))
     print("Multiplication (a*b):", mul(a,b))
+    print("Division (a/b):", div(a,b))
 
 if __name__ == "__main__":
     main()
